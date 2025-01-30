cbb3a8a707d3593a6a31045ede982e777858e202
a.gostev <gostev.a@perfluence.io>
2025-01-30 19:12:21 +0300

    Delete required association teacher-subject -> survey-item

--- src/Presentation/Web/Response/Model/LiteMySurvey.php
+++ src/Presentation/Web/Response/Model/LiteMySurvey.php
@@ -3,24 +3,47 @@
 namespace App\Presentation\Web\Response\Model;
 
 use App\Domain\Entity\MySurvey as DomainMySurvey;
+use App\Domain\Entity\MySurveyItem;
+use App\Domain\Helper\HArray;
 use OpenApi\Attributes as OA;
 
 readonly class LiteMySurvey
 {
+    /**
+     * @param LiteSurvey $survey
+     * @param bool $completed
+     * @param string|null $completed_at
+     * @param TeacherSubject[] $teacher_subjects
+     */
     public function __construct(
         public LiteSurvey $survey,
         public bool $completed,
         #[OA\Property(format: 'date-time')]
         public string|null $completed_at,
+        public array $teacher_subjects,
     ) {
     }
 
     public static function fromMySurvey(DomainMySurvey $survey): self
     {
+        $teacherSubjects = array_values(
+            array_filter(
+                HArray::indexExtended(
+                    $survey->getMyItems()->toArray(),
+                    fn(MySurveyItem $i) => $i->getTeacherSubjectId()?->toRfc4122(),
+                    fn(MySurveyItem $i) => $i->getTeacherSubject(),
+                ),
+            ),
+        );
+
         return new self(
             LiteSurvey::fromMySurvey($survey),
             $survey->isCompleted(),
             $survey->getCompletedAt()?->format(DATE_RFC3339),
+            array_map(
+                TeacherSubject::fromTeacherSubject(...),
+                $teacherSubjects,
+            ),
         );
     }
 }
--- src/Presentation/Web/Response/Model/MySurvey.php
+++ src/Presentation/Web/Response/Model/MySurvey.php
@@ -3,34 +3,46 @@
 namespace App\Presentation\Web\Response\Model;
 
 use App\Domain\Entity\MySurvey as DomainMySurvey;
+use App\Domain\Entity\MySurveyItem;
+use App\Domain\Helper\HArray;
 use OpenApi\Attributes as OA;
 
 readonly class MySurvey
 {
     /**
-     * @param LiteSurvey $survey
+     * @param Survey $survey
      * @param bool $completed
      * @param string|null $completed_at
-     * @param MySurveyItem[] $items
+     * @param TeacherSubject[] $teacher_subjects
      */
     public function __construct(
-        public LiteSurvey $survey,
+        public Survey $survey,
         public bool $completed,
         #[OA\Property(format: 'date-time')]
         public string|null $completed_at,
-        public array $items,
+        public array $teacher_subjects,
     ) {
     }
 
     public static function fromMySurvey(DomainMySurvey $survey): self
     {
+        $teacherSubjects = array_values(
+            array_filter(
+                HArray::indexExtended(
+                    $survey->getMyItems()->toArray(),
+                    fn(MySurveyItem $i) => $i->getTeacherSubjectId()?->toRfc4122(),
+                    fn(MySurveyItem $i) => $i->getTeacherSubject(),
+                ),
+            ),
+        );
+
         return new self(
-            LiteSurvey::fromMySurvey($survey),
+            Survey::fromMySurvey($survey),
             $survey->isCompleted(),
             $survey->getCompletedAt()?->format(DATE_RFC3339),
             array_map(
-                MySurveyItem::fromItem(...),
-                $survey->getMyItems()->toArray(),
+                TeacherSubject::fromTeacherSubject(...),
+                $teacherSubjects,
             ),
         );
     }
