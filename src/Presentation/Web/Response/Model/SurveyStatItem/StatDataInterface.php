4c75ae5d6f9d8706b6c86fe825d0f9b4df097383
a.gostev <gostev.a@perfluence.io>
2025-02-27 12:16:07 +0300

    fix

--- src/Presentation/Web/Response/Model/SurveyStatItem/StatDataInterface.php
+++ src/Presentation/Web/Response/Model/SurveyStatItem/StatDataInterface.php
@@ -5,6 +5,7 @@ namespace App\Presentation\Web\Response\Model\SurveyStatItem;
 use App\Domain\Enum\SurveyItemTypeEnum;
 use App\Presentation\Web\OpenApi\Attribute as LOA;
 use Symfony\Component\Serializer\Attribute\DiscriminatorMap;
+use Symfony\Component\Serializer\Attribute\SerializedName;
 
 #[DiscriminatorMap(
     typeProperty: 'type',
@@ -20,11 +21,15 @@ interface StatDataInterface
     #[LOA\Enum(SurveyItemTypeEnum::class)]
     public function getType(): string;
 
+    #[SerializedName('teacher_name')]
     public function getTeacherName(): string|null;
 
+    #[SerializedName('teacher_id')]
     public function getTeacherId(): string|null;
 
+    #[SerializedName('available_count')]
     public function getAvailableCount(): int;
 
+    #[SerializedName('completed_count')]
     public function getCompletedCount(): int;
 }
