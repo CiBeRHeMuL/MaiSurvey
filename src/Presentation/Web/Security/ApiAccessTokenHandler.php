b837d644a2b0a815874ad16b1b97e2c485bf2b85
Андрей Гостев <gostev.a@perfluence.io>
2025-04-02 13:19:58 +0300

    debug

--- src/Presentation/Web/Security/ApiAccessTokenHandler.php
+++ src/Presentation/Web/Security/ApiAccessTokenHandler.php
@@ -7,6 +7,7 @@ use App\Domain\Service\Jwt\JwtServiceInterface;
 use App\Domain\Service\Jwt\UserJwtClaims;
 use App\Presentation\Web\Security\User\SymfonyUser;
 use DateTimeImmutable;
+use Psr\Log\LoggerInterface;
 use SensitiveParameter;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
@@ -17,6 +18,7 @@ class ApiAccessTokenHandler implements AccessTokenHandlerInterface
     public function __construct(
         private JwtServiceInterface $jwtService,
         private GetUserUseCase $useCase,
+        private LoggerInterface $logger,
     ) {
     }
 
@@ -35,9 +37,12 @@ class ApiAccessTokenHandler implements AccessTokenHandlerInterface
              * @return SymfonyUser|null
              */
             function (string $userIdentifier, array $attributes): SymfonyUser|null {
+                $this->logger->error(json_encode($attributes));
+                $this->logger->error($userIdentifier);
                 $decoded = $attributes['decoded'];
                 $user = $this->useCase
                     ->execute($decoded->getId());
+                $this->logger->info(json_encode($user));
                 if (
                     $user !== null
                     && $user->getAccessToken() === $decoded->getToken()
