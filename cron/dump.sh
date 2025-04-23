76426634c8c457a1f7ed96b34724520caddc9ccc
Андрей Гостев <gostev.a@perfluence.io>
2025-04-23 15:33:55 +0300

    cron

---
+++ cron/dump.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/a_gostev/app/api.mai-survey.ru \
+ && mkdir -p dump >/home/a_gostev/app/api.mai-survey.ru/var/log/cron.log 2>&1 \
+  && touch /home/a_gostev/app/api.mai-survey.ru/dump/dump_$(date +%Y-%m-%d_%H:%i).sql >/home/a_gostev/app/api.mai-survey.ru/var/log/cron.log 2>&1 \
+   && docker exec -it mai-survey-postgresql-container pg_dump --dbname=ms --file="/app/dump/dump_$(date +%Y-%m-%d_%H:%i).sql" --format=c --create --clean --if-exists --verbose -U ms >/home/a_gostev/app/api.mai-survey.ru/var/log/cron.log 2>&1
