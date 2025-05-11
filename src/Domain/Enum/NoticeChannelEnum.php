ff3abffa13e5302697eca631e74eb97c10761fdc
Андрей Гостев <gostev.a@perfluence.io>
2025-05-11 15:34:16 +0300

    notice basics

--- src/Domain/Enum/NoticeChannelEnum.php
+++ src/Domain/Enum/NoticeChannelEnum.php
@@ -5,5 +5,12 @@ namespace App\Domain\Enum;
 enum NoticeChannelEnum: string
 {
     case Telegram = 'telegram';
+
+    public function getName(): string
+    {
+        return match ($this) {
+            self::Telegram => 'Telegram',
+        };
+    }
 }
 
--- src/Domain/Enum/NoticeTypeEnum.php
+++ src/Domain/Enum/NoticeTypeEnum.php
@@ -5,5 +5,12 @@ namespace App\Domain\Enum;
 enum NoticeTypeEnum: string
 {
     case NewSurvey = 'new_survey';
+
+    public function getName(): string
+    {
+        return match ($this) {
+            self::NewSurvey => 'Новые опросы',
+        };
+    }
 }
 
